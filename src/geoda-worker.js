bc94df462b28c7dcc8151cd6042386ae50cc4208
theuscovidatlas <dhalpern@uchicago.edu>
2021-07-05 18:48:16 -0500

    Worker WIP

---
+++ src/geoda-worker.js
@@ -0,0 +1,79 @@
+var GeodaWorkerProxy = `class GeodaWorkerProxy {
+  constructor() {
+    this.geoda = null;
+  } 
+  /**
+   * Initialize the worker with jsgeoda.
+   * Populated all jsgeoda functions in the worker for exposure through Comlink.
+   * @returns {Boolean} True, after loaded.
+   */
+  async New() {
+    if (this.geoda !== null) return true;
+    var geoda = await jsgeoda();
+    this.geoda = geoda;
+    var allFunctions = this.getAllFuncs(this.geoda);
+    for (const key of allFunctions) {
+      this[key] = (...args) => this.handleFunction(key, args);
+    }
+    return true;
+  }
+  
+    // thanks @ https://stackoverflow.com/questions/31054910/get-functions-methods-of-a-class/31055217
+  getAllFuncs(toCheck) {
+    var props = [];
+    var obj = toCheck;
+    do {
+      props = props.concat(Object.getOwnPropertyNames(obj));
+    } while ((obj = Object.getPrototypeOf(obj)) && obj != Object.prototype);
+  
+    return props.sort().filter(function (e, i, arr) {
+      if (e != arr[i + 1] && typeof toCheck[e] == "function") return true;
+    });
+  }
+  /**
+   * Pass through of readGeoJson.
+   * @param {String} url The url of the geojson file to be fetched.
+   * @returns {String} A unique id of the geoda object.
+   * @returns {GeoJson} Fetched geodata
+   */
+  async loadGeoJSON(url, geoIdColumn) {
+    if (this.geoda === null) await this.New();
+    var response = await fetch(url);
+    var responseClone = response.clone();
+    var [geojsonData, ab] = await Promise.all([
+      response.json(),
+      responseClone.arrayBuffer(),
+    ]);
+
+    if (
+      !(isNaN(+geojsonData.features[0].properties[geoIdColumn])) 
+      && "number" !== typeof geojsonData.features[0].properties[geoIdColumn])
+    {   
+      for (var i=0; i<geojsonData.features.length; i++) {
+        geojsonData.features[i].properties[geoIdColumn] = +geojsonData.features[i].properties[geoIdColumn]
+      }
+    }
+
+    var id = this.readGeoJSON(ab);
+    return [id, geojsonData];
+  }
+
+  /**
+   * Worker functions are slightly obfuscated, so this lists out availble Prototype functions.
+   * @returns {Array} List of available functions.
+   */
+  async listFunctions() {
+    if (this.geoda === null) await this.New();
+    return this.getAllFuncs(this);
+  }
+
+  handleFunction(fn, args) {
+    if (["New", "loadGeoJSON", "listFunctions"].includes(fn)) {
+      return this[fn](...args);
+    } else {
+      return this.geoda[fn](...args);
+    }
+  }
+}`
+
+exports["GeodaWorkerProxy"] = GeodaWorkerProxy; 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 // https://gist.github.com/surma/b2705b6cca29357ebea1c9e6e15684cc
 import jsgeoda from './jsgeoda';
 import GeoDaWasm from './geoda-proxy';
-
+import GeodaWorkerProxy from './GeodaWorkerProxy';
+import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
+import Blob from ("cross-blob");
 // jsgeoda_wasm is a global variable that caches the return from
 // initialization of libgeoda WASM module
 // var jsgeoda_wasm = null;
@@ -16,14 +18,30 @@ import GeoDaWasm from './geoda-proxy';
  *
  * @returns {Object} geoda - a GeoDaWasm instance built from WASM
  */
-function New() {
-  return new Promise((resolve) => {
-    jsgeoda().then((wasm) => {
-      // jsgeoda_wasm = wasm;
-      const geoda = new GeoDaWasm(wasm);
-      resolve(geoda);
+function New({
+  useWorker=false
+}) {
+  if (useWorker){
+    const blob = new Blob([
+        'importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");var exports={};',
+        jsgeoda.toString(), 
+        GeodaWorkerProxy,
+        'const geodaWorker = new GeodaWorkerProxy();Comlink.expose(geodaWorker);'
+      ], 
+      { type: 'text/javascript' });
+    const url = URL.createObjectURL(blob);
+    const worker =  new Worker(url);
+    return Comlink.wrap(worker);
+  } else {
+    return new Promise((resolve) => {
+      jsgeoda().then((wasm) => {
+        // jsgeoda_wasm = wasm;
+        const geoda = new GeoDaWasm(wasm);
+        resolve(geoda);
+      });
     });
-  });
+
+  }
 }
 
 exports.New = New;
